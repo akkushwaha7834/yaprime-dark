8c4c24c31708f769a429de50d1d9c67348db14f4
akkushwaha7834 <akkushwaha018@gmail.com>
2024-03-26 15:25:15 +0530

    banner created, menu created

---
+++ include/header.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Start Your Own Forex Brokerage | Forex Broker Services</title>
+	<link rel="shortcut icon" href="./images/favicon.png" type="image/x-icon">
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="./vendor/font-awesome/css/all.min.css">
+	<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+</head>
+<body>
+<?php include_once('./include/trading-view-widget.php') ?>
+
+<header class="header navbar-dark">
+ <nav class="navbar navbar-expand-lg py-0">
+    <div class="container">
+       <a class="navbar-brand" href="index.php">
+       <img src="images/logo.webp" alt="Logo" title="YaPrime Logo">
+       </a>
+       <div>
+          <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+       </div>
+       <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+          <div class="offcanvas-header">
+             <a class="navbar-brand" href="/">
+             <img src="images/logo.png" alt="Logo" title="YaPrime Logo">
+             </a>
+             <button type="button" class="btn-close invert" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+             <div class="navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-lg-auto">
+                   <li class="nav-item">
+                      <a href="index.php" class="nav-link">Home</a>
+                   </li>
+                   <li class="nav-item">
+                      <a class="nav-link" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                         About
+                         <svg class="ms-1" width="12" height="6" viewBox="0 0 12 6" fill="black" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 1L5.99999 5L1 1" stroke="white" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"></path>
+                         </svg>
+                      </a>
+                      <div class="dropdown-menu">
+                         <div class="container">
+                            <button class="border-0 bg-transparent text-white mb-3 d-block d-lg-none">
+                            <i class="fa-solid fa-chevron-left"></i>
+                            <span>Back</span>
+                            </button>
+                            <div class="h-vh-80 overflow-y-auto overflow-x-hidden">
+                               <ul class="list-unstyled row gx-5 gy-4">
+                                  <div class="col-lg-12">
+                                     <div class="h6 text-uppercase mb-4">ABOUT YaPrime</div>
+                                     <div class="container">
+                                        <div class="row">
+                                           <div class="col-lg-4">
+                                              <ol class="list-unstyled row gx-0 mx-0 mb-3 drop-inner-links">
+                                                 <li class="col-12 mb-2">
+                                                    <a href="about-us.php">About us</a>
+                                                 </li>
+                                                 <li class="col-12 mb-2">
+                                                    <a href="careers.php">Careers</a>
+                                                 </li>
+                                                 <li class="col-12 mb-2">
+                                                    <a href="contact-us.php">Contact Us</a>
+                                                 </li>
+                                              </ol>
+                                           </div>
+                                           <div class="col-lg-4">
+                                              <ol class="list-unstyled row gx-0 mx-0 mb-3 drop-inner-links">
+                                                 <li class="col-12 mb-2">
+                                                    <a href="why-choose-us.php">Why Choose Us</a>
+                                                 </li>
+                                                 <li class="col-12 mb-2">
+                                                    <a href="portfolio.php">Portfolio</a>
+                                                 </li>
+                                                 <li class="col-12 mb-2">
+                                                    <a href="our-teams.php">Meet Our Team</a>
+                                                 </li>
+                                              </ol>
+                                           </div>
+                                           <div class="col-lg-4">
+                                              <ol class="list-unstyled row gx-0 mx-0 mb-3 drop-inner-links">
+                                                 <li class="col-12 mb-2">
+                                                    <a href="blog.php">Our Blog</a>
+                                                 </li>
+                                              </ol>
+                                           </div>
+                                        </div>
+                                     </div>
+                                  </div>
+                               </ul>
+                            </div>
+                         </div>
+                      </div>
+                   </li>
+                   <li class="nav-item">
+                      <a href="liquidity.php" class="nav-link" role="button" aria-expanded="false">
+                         Liquidity
+                      </a>
+                      
+                   </li>
+                   <li class="nav-item">
+                      <a href="broker-setup.php" class="nav-link" role="button" aria-expanded="false">
+                         Broker Setup
+                      </a>
+                      
+                   </li>
+                   <li class="nav-item">
+                      <a href="institutional-clients.php" class="nav-link" role="button"  aria-expanded="false">
+                      Institutional Clients
+                      </a>
+                   </li>
+                   <li class="nav-item">
+                      <a href="metatrader-support.php" class="nav-link" role="button" aria-expanded="false">
+                      Metatrader Support
+                      </a>
+                   </li>
+                   <li class="nav-item">
+                      <a href="blog.php" class="nav-link">Blog</a>
+                   </li>
+                   <li class="nav-item">
+                      <a href="contact-us.php" class="nav-link">Contact us</a>
+                   </li>
+                </ul>
+                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Request Quote</button>
+             </div>
+          </div>
+       </div>
+    </div>
+ </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,86 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="home-banner">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-10">
+				<div class="banner_content">
+					<div class="banner_shape">
+						<img src="images/2.webp" class="banner-shape img-fluid">
+					</div>
+					<div class="banner_shapee">
+						<img src="images/2.webp" class="banner-shape img-fluid">
+					</div>
+					<h1 class="pointer_heading ">Best Liquidity Solutions For Your Forex Brokerage</h1>
+					<p class="banner_para">Are you ready to venture into the dynamic and potentially lucrative world of Forex brokerage?</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</section>
+
+<section class="trading_indices">
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-md-3">
+        <div class="image_box">
+          <div class="image_col">
+            <div class="image_caption">
+              <center>
+              <lottie-player src="images/lottie-json/forex.json" style="width: 100px; height: 100px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+              </center>
+              <h3 class="factoids_number">30+</h3>
+              <h4>Forex</h4>
+              <p>Happy Customers serving their Fullfledged Service to Connect traders.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="image_box">
+          <div class="image_col">
+            <div class="image_caption">
+            <center>
+              <lottie-player src="images/lottie-json/indices.json" style="width: 100px; height: 100px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+            </center>
+              <h3 class="factoids_number">1.2+</h3>
+              <h4>Indices</h4>
+              <p>More than Million traders booking their trades Peacefully.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="image_box">
+          <div class="image_col">
+            <div class="image_caption">
+            <center>
+              <lottie-player src="images/lottie-json/gold.json" style="width: 100px; height: 100px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+            </center>
+              <h3 class="factoids_number">2+</h3>
+              <h4>Commodities</h4>
+              <p>Know Why YaPrime, Technology Pioneer-Technically, Best in the market.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <div class="image_box">
+          <div class="image_col">
+            <div class="image_caption">
+            <center>
+              <lottie-player src="images/lottie-json/stocks.json" style="width: 100px; height: 100px" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+            </center>
+              <h3 class="factoids_number">300+</h3>
+              <h4>Stocks</h4>
+              <p>More than Million traders booking their trades Peacefully.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
