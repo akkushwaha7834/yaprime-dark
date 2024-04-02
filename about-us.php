fba1373231b815242446e076e86bf0e401f34940
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-02 12:57:55 +0530

    Aboiut banner, About content section created

---
+++ about-us.php
@@ -0,0 +1,179 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="about_banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="banner_content text-center">
+					<h4 class="text-primary">
+						About Us
+					</h4>
+					<h1 class="text-white about-heading">
+						YaPrime: Inspiring Future Forex Brokers
+					</h1>
+					<p class="text-white">
+						At YaPrime we understand the importance of seamless and efficient Metatrader 4/5 trading platform administration for your trading endeavors. With years of experience in the financial technology industry, we have established ourselves as a trusted partner for traders seeking reliable Metatrader administration support.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="about-content-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="about-box justify-content-center align-items-center d-flex">
+					<div class="about-box-bg"></div>
+					<img src="images/work-process.png" class="rotation-img" alt="Work Process">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<h4 class="text-primary">
+					About Us
+				</h4>
+				<h3 class="text-white">
+					Bespoke Liquidity Solutions Provider
+				</h3>
+				<p class="text-white">
+					YaPrime, a brand of Ya Group Limited, is a forex and CFD liquidity solutions provider focused on institutional clients. It was founded by industry veterans with deep knowledge of the retail and institutional sectors. YaPrime's team of experts and broker consultants helps clients set up their own forex brokerage companies and provides exceptional liquidity solutions and unparalleled support.
+				</p>
+				<ul class="check-list">
+					<li>
+						<span>
+							<i class="fa fa-check" aria-hidden="true"></i>
+						</span>
+						<p class="text-white">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+						</p>
+					</li>
+					<li>
+						<span>
+							<i class="fa fa-check" aria-hidden="true"></i>
+						</span>
+						<p class="text-white">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+						</p>
+					</li>
+					<li>
+						<span>
+							<i class="fa fa-check" aria-hidden="true"></i>
+						</span>
+						<p class="text-white">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+						</p>
+					</li>
+					<li>
+						<span>
+							<i class="fa fa-check" aria-hidden="true"></i>
+						</span>
+						<p class="text-white">
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+						</p>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="about-bridge">
+    <div class="container">
+        <div class="row gy-5">
+            <div class="col-lg-8 offset-lg-2 text-center">
+                <h2 class="text-primary">Your Bridge to Forex Success</h2>
+                <p class="mb-5 text-white">Discover how our solutions empower your journey</p>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/growth.png" alt="Grow Your Forex Business" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Grow Your Forex Business</h5>
+                        <p class="card-text">We're dedicated to boosting your forex business by delivering efficiencies that benefit both you and your clients.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/scaling.png" alt="Access Global Liquidity Pools" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Access Global Liquidity&nbsp;Pools</h5>
+                        <p class="card-text">Our custom liquidity solutions leverage fast, robust technology and institutional risk management and execution, giving you access to global liquidity pools.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/simplifying.png" alt="Scale Your Trading Offering" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Scale Your Trading Offering</h5>
+                        <p class="card-text">With the ability to distribute and source deals globally across five continents, we enable you to scale your trading offering and expand your reach.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/global.png" alt="Simplify Cost and Complexity" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Simplify Cost and Complexity</h5>
+                        <p class="card-text">Reduce cost and complexity by leveraging our third-party integrations and trading infrastructure. This allows you to focus on delivering your core value.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/customise-and-personalize.png" alt="Customise and Personalize" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Customise and Personalize</h5>
+                        <p class="card-text">Build apps and personalise your services with ease. Access our API solutions to create fully integrated experiences, capture client data, or brand our frontends according to your preferences.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/expertise-in-global-expansion.png" alt="Expertise in Global Expansion" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Expertise in Global Expansion</h5>
+                        <p class="card-text">We provide the expertise you need to scale your business across global markets, ensuring you have the support required for your brokerage success.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/trading-technology.png" alt="Top-Tier Trading Technology" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Top-Tier Trading Technology</h5>
+                        <p class="card-text">Explore our best-in-class trading technology solutions, including customisable trading platforms and APIs. These solutions are designed to stay at the forefront of industry innovation.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card border-0 shadow position-relative text-center h-100 card-hover">
+                    <div class="card-icon">
+                        <img src="https://www.yaprime.com/old/public/web/images/access-to-global-markets.png" alt="Access to Global Markets" class="img-fluid">
+                    </div>
+                    <div class="card-body mt-5">
+                        <h5 class="card-title">Access to Global Markets</h5>
+                        <p class="card-text">Our platform opens doors to global markets through our proprietary liquidity pool, aggregating pricing from multiple top-tier liquidity banks and ECNs. Enjoy deep liquidity and competitive pricing across a diverse range of financial instruments.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include_once('./include/footer.php') ?>
