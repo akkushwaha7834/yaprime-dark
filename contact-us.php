1ec71aefd5f92482f49e0b6d9005e696254a1de7
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-02 16:10:41 +0530

    contact page created

---
+++ contact-us.php
@@ -0,0 +1,19 @@
+<?php include_once('./include/header.php') ?>
+
+<!-- Banner -->
+<section class="contact_banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="contact_content text-center">
+					<h1 class="text-primary">Contact Us</h1>
+					<p class="text-white">
+						YaPrime, a brand of Ya Group Limited, is a forex and CFD liquidity solutions provider focused on institutional clients. 
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
