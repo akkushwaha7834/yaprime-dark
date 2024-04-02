39b9fa8d852b872d8b6e37c4f1e41a950267bfbb
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-02 16:59:59 +0530

    form added

--- contact-us.php
+++ contact-us.php
@@ -16,4 +16,48 @@
 	</div>
 </section>
 
+<section class="contact-content-section">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-4">
+				<figure class="mb-0 border-anim mx-auto">
+	              <lottie-player src="images/lottie-json/banner.json" style="width: 250px; height: 250px"
+	              background="transparent" speed="1" direction="1" mode="normal" loop=""
+	              autoplay="">
+	              </lottie-player>
+	            </figure>
+			</div>
+			<div class="col-md-1"></div>
+			<div class="col-md-7">
+				<div class="contact-form">
+					<h2 class="mb-2 text-primary">Talk to us!</h2>
+					<p class="text-white">Get your exclusive invite to speak with our team of Forex experts!</p>
+					<form class="form-group mt-3 mt-sm-4 mb-0">
+						<div class="row">
+							<div class="col-6 mb-3">
+								<input class="form-control form-item" type="text" placeholder="Name">
+							</div>
+							<div class="col-6 mb-3">
+								<input class="form-control form-item" type="email" placeholder="Email">
+							</div>
+							<div class="col-6 mb-3">
+								<input class="form-control form-item" type="text" placeholder="Mobile">
+							</div>
+							<div class="col-6 mb-3">
+								<input class="form-control form-item" type="text" placeholder="Location">
+							</div>
+							<div class="col-12 mb-3">
+								<textarea class="form-control form-item" placeholder="Message"></textarea>
+							</div> 
+							<div class="col-12">
+								<button class="btn-4 mt-3">Submit Now</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <?php include_once('./include/footer.php') ?>
