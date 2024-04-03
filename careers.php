0c9e024e9773dc26f05c5275e22d2e6e782f244c
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-03 16:16:31 +0530

    Careers Open Positions aded

--- careers.php
+++ careers.php
@@ -36,5 +36,132 @@
 		</div>
 	</div>
 </section>
-
+<section class="careers_values">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2 class="mb-3 text-primary text-center">Our Values</h2>
+				<p class="text-white mb-5 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="careers_image_box">
+					<div class="careers_image_content mb-3">
+						<img src="images/proudness.png" class="img-fluid">
+						<h4>Take pride</h4>
+					</div>
+					<div class="careers_content_image">
+						<p>At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="career_openings">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h3 class="text-primary">Open positions</h3>
+				<p class="text-white">Let’s create clever things together! Are you up for it? Let us know!</p>
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col-md-12 careers_right_card">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-md-10">
+							<h2 class="careers_position">
+								Senior laravel developer
+							</h2>
+							<p class="careers_pos_details">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.<br>
+							</p>
+						</div>
+						<div class="col-md-2">
+							<a href="career-details.php" class="apply_now_button">
+							Apply Now
+						</a>
+						</div>
+					</div>
+				</div>			
+			</div>
+		</div>
+		<div class="row mt-3">
+			<div class="col-md-12 careers_right_card">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="col-md-10">
+							<h2 class="careers_position">
+								Senior laravel developer
+							</h2>
+							<p class="careers_pos_details">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.<br>
+							</p>
+						</div>
+						<div class="col-md-2">
+							<a href="career-details.php" class="apply_now_button">
+							Apply Now
+						</a>
+						</div>
+					</div>
+				</div>			
+			</div>
+		</div>
+	</div>
+</section>
 <?php include_once('./include/footer.php') ?>
