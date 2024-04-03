43dc335b712ec1e6428a8ec170b13fbf355eef78
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-03 16:02:21 +0530

    Careers Content section added

--- careers.php
+++ careers.php
@@ -1,20 +1,37 @@
 <?php include_once('./include/header.php') ?>
 
-<section class="about_banner">
+<section class="careers_banner">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<div class="banner_content text-center">
 					<h4 class="text-primary">
-						About Us
+						Careers
 					</h4>
 					<h1 class="text-white about-heading">
 						YaPrime: Inspiring Future Forex Brokers
 					</h1>
 					<p class="text-white">
-						At YaPrime we understand the importance of seamless and efficient Metatrader 4/5 trading platform administration for your trading endeavors. With years of experience in the financial technology industry, we have established ourselves as a trusted partner for traders seeking reliable Metatrader administration support.
+						At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.
 					</p>
 				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="careers_content">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<h3 class="text-primary">
+					Join Us Today
+				</h3>
+				<p class="text-white">
+					At YaPrime, we're all about learning, growing, and innovating together as a team. As a company, we're committed to progress – for our customers and employees. We welcome those with creativity, drive, agility, and a passion for thinking differently.
+				</p>
+			</div>
+			<div class="col-md-6">
+				<lottie-player src="images/lottie-json/careers.json" style="width: 80%; height: 80%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
 			</div>
 		</div>
 	</div>
