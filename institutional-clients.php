71221bbf1a872d96c963042eb834c3680c7b07eb
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-08 16:12:09 +0530

    Institutional Clients Page done

--- institutional-clients.php
+++ institutional-clients.php
@@ -1,6 +1,6 @@
 <?php include_once('./include/header.php') ?>
 
-<section class="about_banner">
+<section class="institutional_clients_banner">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
