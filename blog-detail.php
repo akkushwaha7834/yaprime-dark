3dfb03a0f8f36869929ce4213f29a5a27dbe198b
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-08 11:53:46 +0530

    blog details file created

---
+++ blog-detail.php
@@ -0,0 +1,23 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="about_banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="banner_content text-center">
+					<h4 class="text-primary">
+						About Us
+					</h4>
+					<h1 class="text-white about-heading">
+						YaPrime: Inspiring Future Forex Brokers
+					</h1>
+					<p class="text-white">
+						At YaPrime we understand the importance of seamless and efficient Metatrader 4/5 trading platform administration for your trading endeavors. With years of experience in the financial technology industry, we have established ourselves as a trusted partner for traders seeking reliable Metatrader administration support.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
