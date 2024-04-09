a597e3daf6bec83a281cb24dae14139c32fa7428
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-09 15:07:54 +0530

    Liquidity Pointers added

--- liquidity.php
+++ liquidity.php
@@ -282,11 +282,89 @@
             </div>
           </div>
           <div class="row">
-            <div class="col-md-12 text-center mt-4">
+            <div class="col-md-12 text-center mt-5">
               <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Request Quote</button>
             </div>
           </div>
         </div>
 	</div>        
+</section>
+<section class="liquidity_pointers">
+    <div class="container">
+        <div class="row gy-4 align-items-center">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="careers_image_box image-box">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center gap-3 mb-3">
+                                    <span>
+                                        <img src="images/vvipimg7.svg" alt="" class="img-fluid">
+                                    </span>
+                                    <h5 class="card-title fw-semibold mb-0">Lightening Fast Brokerage Setup</h5>
+                                </div>
+                                <p class="card-text">Prime Special offers a complete and well-rounded setup for your brokerage. It equips you with all the necessary tools, accounts, and systems, ensuring you're ready to dive into the Forex market instantly. </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="careers_image_box image-box">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center gap-3 mb-3">
+                                    <span>
+                                        <img src="images/vvipimg8.svg" alt="" class="img-fluid">
+                                    </span>
+                                    <h5 class="card-title fw-semibold mb-0">Effortless Sub-Account Management</h5>
+                                </div>
+                                <p class="card-text">With the integrated MAM plugin, clients can effortlessly manage multiple sub-accounts from a master account. This simplifies trading operations, efficiently handling clients' accounts and optimising trading strategies.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <figure class="mb-0 border-anim mx-auto">
+	              <lottie-player src="images/lottie-json/banner.json" style="width: 300px; height: 300px" background="transparent" speed="1" direction="1" mode="normal" loop=""autoplay="">
+	              </lottie-player>
+	            </figure>
+            </div>
+            <div class="col-md-6">
+                <figure class="mb-0 border-anim mx-auto">
+	              <lottie-player src="images/lottie-json/banner.json" style="width: 300px; height: 300px" background="transparent" speed="1" direction="1" mode="normal" loop=""autoplay="">
+	              </lottie-player>
+	            </figure>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="careers_image_box image-box">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center gap-3 mb-3">
+                                    <span>
+                                        <img src="images/vvipimg9.svg" alt="" class="img-fluid">
+                                    </span>
+                                    <h5 class="card-title fw-semibold mb-0">Mobile Accessibility</h5>
+                                </div>
+                                <p class="card-text">Prime Special provides the advantage of mobile management. Clients can stay connected and monitor their brokerage activities on the go, ensuring they never miss an important update or trading opportunity.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="careers_image_box image-box">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center gap-3 mb-3">
+                                    <span>
+                                        <img src="images/vvipimg8.svg" alt="" class="img-fluid">
+                                    </span>
+                                    <h5 class="card-title fw-semibold mb-0">Seamless Integration</h5>
+                                </div>
+                                <p class="card-text">The inclusion of the Fix API manager allows for seamless integration with external systems and liquidity providers. This enhances the flexibility and connectivity of clients' brokerages, streamlining their operations for success.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <?php include_once('./include/footer.php') ?>
