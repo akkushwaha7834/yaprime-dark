a6ba9462e28db3b28d224dbf639fc314a66be0db
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-09 11:03:16 +0530

    our team file created

---
+++ our-team.php
@@ -0,0 +1,23 @@
+<?php include_once('./include/header.php') ?>
+
+<section class="team_banner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="banner_content text-center">
+					<h4 class="text-primary">
+						Our Team
+					</h4>
+					<h1 class="text-white about-heading">
+						Our Expert Crew Meet Our Leadership
+					</h1>
+					<p class="text-white">
+						Est sit amet facilisis magna etiam tempor orci eu lobortis. Eu nisl nunc mi ipsum faucibus vitae.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include_once('./include/footer.php') ?>
