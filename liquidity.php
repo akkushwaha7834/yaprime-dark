e9816fa62a9ccdccdb76f6158039c170baef1a9a
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-09 13:26:19 +0530

    Liquidity Page new section addedd

--- liquidity.php
+++ liquidity.php
@@ -215,4 +215,78 @@
       </div>
    </div>
 </section>
+<section class="liquidity_benefits_section">
+	<div class="banner_content">
+		<div class="container">
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <h2 class="text-primary">
+                YaPrime VIP Account Benefits
+              </h2>
+              <h3 class="text-white">
+                Benefits of Using Our Prime VIP Account
+              </h3>
+              <p class="text-center text-white">
+                Our Prime VIP accounts offer exclusive benefits that set it apart from the competition. It provides our clients the opportunity to access an inclusive set of trading accounts for active trading right after the initial deposit.
+              </p>
+            </div>
+          </div>
+          <div class="row gy-4 mt-3">
+            <div class="col-md-6">
+              <div class="image-box">
+                <div class="image_content_box mt-0 mb-3">
+                  <img src="images/primevipimage.png" class="img-fluid">
+                  <h4>
+                    Range of Instrument Options
+                  </h4>
+                </div>
+                <p class="mb-0">YaPrime’s VIP Forex Trading Account holders have access to a wide range of trading instruments listed on our website. Moreover, they can request additional instruments suiting their trading portfolio.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image-box">
+                <div class="image_content_box mt-0 mb-3">
+                  <img src="images/primevipimage2.png" class="img-fluid">
+                  <h4>
+                    Tighter Spreads
+                  </h4>
+                </div>
+                <p class="mb-0">Enjoy trading with tighter spreads, zero commission, and the option for swap-free accounts. These favourable conditions contribute to a cost-efficient and flexible trading experience.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image-box">
+                <div class="image_content_box mt-0 mb-3">
+                  <img src="images/primevipimage3.png" class="img-fluid">
+                  <h4>
+                    Higher Leverage and Execution
+                  </h4>
+                </div>
+                <p class="mb-0">Get access to higher leverage options for amplified trading power and better execution, enabling VIP Trading Account holders to grab trading opportunities with precision and efficiency.
+                </p>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="image-box">
+                <div class="image_content_box mt-0 mb-3">
+                  <img src="images/primevipimage4.png" class="img-fluid">
+                  <h4>
+                    Designed with Expert Insights
+                  </h4>
+                </div>
+                <p class="mb-0">YaPrime's account packages were carefully designed in collaboration with experienced traders and Forex industry experts. Benefit from their invaluable insights to kickstart your forex brokerage business.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 text-center mt-4">
+              <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Request Quote</button>
+            </div>
+          </div>
+        </div>
+	</div>        
+</section>
 <?php include_once('./include/footer.php') ?>
