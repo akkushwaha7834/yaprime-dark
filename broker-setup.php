bb33d5cc952f67ef5d48066ff7e75107a2f8aee8
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-10 11:41:35 +0530

    CRM & API section added in Broker setup page

--- broker-setup.php
+++ broker-setup.php
@@ -20,4 +20,120 @@
 	</div>
 </section>
 
+<section class="crm_api_section">
+	<div class="container">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h4 class="text-primary">
+		            CRM & API
+		          </h4>
+				<h2 class="text-white">CRM &amp; API Integration Solutions</h2>
+				<p class="text-white">
+					Contact us to explore how our CRM and API integration can empower your business and provide your clients and partners with a unique trading experience.
+				</p>
+			</div>
+		</div>
+		<div class="row align-items-center justify-content-center mt-4">
+			<div class="col-md-6">
+	          <h4 class="text-primary">
+	            About CRM
+	          </h4>
+	          <h2 class="text-white">
+	            What is a CRM?
+	          </h2>
+	          <p class="text-white">
+	            CRM stands for Customer Relationship Management. It's a sophisticated system designed to help businesses like yours build and nurture strong client relationships while effectively managing partner networks. What are the benefits of using CRM, you ask?
+	          </p>
+	          <div class="image-box">
+	            <div class="image_content_box mb-3">
+	            	<img src="images/imgg2.png" class="crm_image_box_one">
+	            	<h4 class="text-primary">Helps to Streamline Client Data</h4>
+	            </div>
+	            <p>CRM enables you to collect, organise, and access crucial client data in one centralised platform. This ensures that no essential information slips through the cracks.</p>
+	          </div>
+	          <div class="image-box mt-4">
+	          	<div class="image_content_box mb-3">
+	          		<img src="images/imgg1.png" class="crm_image_box_one">
+	            	<h4 class="text-primary">Optimises Communication</h4>
+	            </div>
+	            <p>Stay in constant contact with your clients. CRM tools facilitate efficient communication through emails, notifications, and other channels.</p>
+	          </div>
+	          <div class="image-box mt-4">
+	          	<div class="image_content_box mb-3">
+	          		<img src="images/imgg3.png" class="crm_image_box_one">
+	            	<h4 class="text-primary">Improves Client Experience</h4>
+	          	</div>
+	            <p>By understanding your clients' trading behaviours, you can design your services to meet their specific needs. Personalisation leads to higher client satisfaction.</p>
+	          </div>
+	          <div class="row liquidity_row_two">
+	              <div class="col-md-6">
+	                <!-- <a class="unique_anchor_one" data-bs-toggle="modal" data-bs-target="#exampleModal">Get Started </a> -->
+	                <button type="button" class="btn btn-warning mt-4" data-bs-toggle="modal" data-bs-target="#exampleModal">Get Started</button>
+	              </div>
+	            </div>
+	        </div>
+	        <div class="col-md-6">
+	          <lottie-player src="images/lottie-json/careers.json" style="width: 100%; height: 100%" background="transparent" speed="1" direction="1" mode="normal" loop="" autoplay=""></lottie-player>
+	        </div>
+		</div>
+	</div>
+</section>
+<section class="liquidity_faqs">
+	<div class="banner_content">
+		<div class="container">
+			<div class="row text-center">
+				<div class="col-md-12">
+					<h4 class="text-primary">
+						FAQS
+					</h4>
+					<h2 class="text-white">
+						Frequently Asked Questions
+					</h2>
+				</div>
+			</div>
+			<div class="row mt-5 align-items-center justify-content-center">
+				<div class="col-md-8">
+					<div class="accordion faq-accordion" id="accordionExample">
+			          <div class="accordion-item border-0 mb-2">
+			            <h2 class="accordion-header">
+			              <button class="accordion-button shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			                What is the one-time setup fee for the Prime VVIP Package?
+			              </button>
+			            </h2>
+			            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+			              <div class="accordion-body">
+			                <p>The one-time setup fee for our Prime VVIP Package is $6000. </p>
+			              </div>
+			            </div>
+			          </div>
+			          <div class="accordion-item border-0 mb-2">
+			            <h2 class="accordion-header">
+			              <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+			                What distinguishes the Prime VVIP Package from the VIP Package?
+			              </button>
+			            </h2>
+			            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+			              <div class="accordion-body">
+			                <p>The Prime VVIP Package not only includes all the features of the VIP Package but also provides additional benefits such as Web API, Forex CRM, Bonus plugin, Extended dealer, Multi-level IB, and Liquidity manager.</p>
+			              </div>
+			            </div>
+			          </div>
+			          <div class="accordion-item border-0">
+			            <h2 class="accordion-header">
+			              <button class="accordion-button collapsed shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+			                How can I upgrade to the Prime VVIP Package?
+			              </button>
+			            </h2>
+			            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+			              <div class="accordion-body">
+			                <p>To upgrade to the Prime VVIP Package, please get in touch with our dedicated support team. They will guide you through the process and help you make the most of this exclusive offering.</p>
+			              </div>
+			            </div>
+			          </div>
+			        </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 <?php include_once('./include/footer.php') ?>
