976565f08b4e57f1098a02c3ca1b43f107cb142e
akkushwaha7834 <akkushwaha018@gmail.com>
2024-04-19 12:53:48 +0530

    Portfolio page done

--- portfolio.php
+++ portfolio.php
@@ -20,6 +20,123 @@
 	</div>
 </section>
 <section class="portfolio-content">
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<h4 class="text-primary">Our Portfolio</h4>
+				<h2 class="text-white">Lorem Ipsum</h2>
+			</div>
+		</div>
+		<div class="row gx-5 mt-5">
+			<div class="col-md-6">
+				<div class="portfolio-list">
+					<a href="portfolio-details.php">
+						<img src="https://frisk.themegenix.com/wp-content/uploads/2024/02/portfolio03.webp" class="img-fluid">
+						<h2 class="text-primary mt-4">
+							Lorem Ipsum
+						</h2>
+					</a>
+					<p class="text-white">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<span class="know_more">
+						<a href="portfolio-details.php">
+							Learn More <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 11L11.2 1.8M12 11V1H2" stroke="currentColor" stroke-width="2" stroke-linecap="square"></path></svg>
+						</a>
+					</span>
+				</div>
+			</div>
+			<div class="col-md-6 mt-5">
+				<div class="portfolio-list">
+					<a href="portfolio-details.php">
+						<img src="https://frisk.themegenix.com/wp-content/uploads/2024/02/portfolio03.webp" class="img-fluid">
+						<h2 class="text-primary mt-4">
+							Lorem Ipsum
+						</h2>
+					</a>
+					<p class="text-white">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<span class="know_more">
+						<a href="portfolio-details.php">
+							Learn More <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 11L11.2 1.8M12 11V1H2" stroke="currentColor" stroke-width="2" stroke-linecap="square"></path></svg>
+						</a>
+					</span>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-list">
+					<a href="portfolio-details.php">
+						<img src="https://frisk.themegenix.com/wp-content/uploads/2024/02/portfolio03.webp" class="img-fluid">
+						<h2 class="text-primary mt-4">
+							Lorem Ipsum
+						</h2>
+					</a>
+					<p class="text-white">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<span class="know_more">
+						<a href="portfolio-details.php">
+							Learn More <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 11L11.2 1.8M12 11V1H2" stroke="currentColor" stroke-width="2" stroke-linecap="square"></path></svg>
+						</a>
+					</span>
+				</div>
+			</div>
+			<div class="col-md-6 mt-5">
+				<div class="portfolio-list">
+					<a href="portfolio-details.php">
+						<img src="https://frisk.themegenix.com/wp-content/uploads/2024/02/portfolio03.webp" class="img-fluid">
+						<h2 class="text-primary mt-4">
+							Lorem Ipsum
+						</h2>
+					</a>
+					<p class="text-white">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<span class="know_more">
+						<a href="portfolio-details.php">
+							Learn More <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 11L11.2 1.8M12 11V1H2" stroke="currentColor" stroke-width="2" stroke-linecap="square"></path></svg>
+						</a>
+					</span>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="portfolio-list">
+					<a href="portfolio-details.php">
+						<img src="https://frisk.themegenix.com/wp-content/uploads/2024/02/portfolio03.webp" class="img-fluid">
+						<h2 class="text-primary mt-4">
+							Lorem Ipsum
+						</h2>
+					</a>
+					<p class="text-white">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+					</p>
+					<span class="know_more">
+						<a href="portfolio-details.php">
+							Learn More <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 11L11.2 1.8M12 11V1H2" stroke="currentColor" stroke-width="2" stroke-linecap="square"></path></svg>
+						</a>
+					</span>
+				</div>
+			</div>
+			<div class="col-md-6 mt-5">
+				<div class="portfolio-list">
+					<a href="portfolio-details.php">
+						<img src="https://frisk.themegenix.com/wp-content/uploads/2024/02/portfolio03.webp" class="img-fluid">
+						<h2 class="text-primary mt-4">
+							Lorem Ipsum
+						</h2>
+					</a>
+					<p class="text-white">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+					</p>
+					<span class="know_more">
+						<a href="portfolio-details.php">
+							Learn More <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 11L11.2 1.8M12 11V1H2" stroke="currentColor" stroke-width="2" stroke-linecap="square"></path></svg>
+						</a>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
 <?php include_once('./include/footer.php') ?>
